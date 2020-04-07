4d17df5d9195ad5ef0fe54c8af358989a5443610
James Giraldo <50636786+JamesGiraldo@users.noreply.github.com>
2020-04-07 00:54:34 -0500

    Modulo Proveedores
    Listo SOU SOU

--- resources/js/app.js
+++ resources/js/app.js
@@ -4,7 +4,7 @@ window.Vue = require('vue');
 Vue.component('categoria', require('./components/Categoria.vue').default);
 Vue.component('articulo', require('./components/Articulo.vue').default);
 Vue.component('cliente', require('./components/Cliente.vue').default);
-
+Vue.component('proveedor', require('./components/Proveedor.vue').default);
 const app = new Vue({
     el: '#app',
     data :{
